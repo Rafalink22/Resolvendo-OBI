246c643d1cae99b4566ecae46b943308458f80af
Bruno Rafael <01804563@sempreuninassau.com.br>
2025-07-24 13:34:37 -0300

    Update contas.py

--- OBI - SENIOR/OBI 2023/contas.py
+++ OBI - SENIOR/OBI 2023/contas.py
@@ -1,17 +1,18 @@
-V = int(input())
-A = int(input())
-F = int(input())
-P = int(input())
+V = int(input()) #Valor disponível
+A = int(input()) #Conta do Açougue
+F = int(input()) #Conta da Farmácia
+P = int(input()) #Conta da Padaria
 
-contas = [A, F, P]
+contas = [A, F, P] #Lista das contas
 
-contas.sort()
+contas.sort() #Ordena as contas
 
+#Verifica quantas contas podem ser pagas
 if contas[0] + contas[1] + contas[2] <= V:
-    print(3)
+    print(3) #Todas as 3 contas
 elif contas[0] + contas[1] <= V:
-    print(2)
+    print(2) #Duas menores contas
 elif contas[0] <= V:
-    print(1)
+    print(1) #A menor conta
 else:
-    print(0)
+    print(0) #Nenhuma conta
