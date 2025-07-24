19427f42d96ab50e8a1bde1e616818f195590686
Bruno Rafael <01804563@sempreuninassau.com.br>
2025-07-24 13:36:11 -0300

    Update subsequencia.py

--- OBI - SENIOR/OBI 2023/subsequencia.py
+++ OBI - SENIOR/OBI 2023/subsequencia.py
@@ -1,25 +1,25 @@
-A = int(input())  # Tamanho da lista1 (SA)
-B = int(input())  # Tamanho da lista2 (SB)
+A = int(input())  #Tamanho da lista1 (SA)
+B = int(input())  #Tamanho da lista2 (SB)
 
 lista1 = []
 lista2 = []
 
-# Lendo a lista SA
+#Lendo a lista SA
 for _ in range(A):
     lista1.append(int(input()))
 
-# Lendo a lista SB
+#Lendo a lista SB
 for _ in range(B):
     lista2.append(int(input()))
 
-j = 0  # Índice da lista2 (SB)
+j = 0  #Índice da lista2 (SB)
 
-# Percorrer lista1 (SA)
+#Percorrer lista1 (SA)
 for item in lista1:
     if j < B and item == lista2[j]:
-        j += 1  # Avança na SB se achar o próximo valor da subsequência
+        j += 1  #Avança na SB se achar o próximo valor da subsequência
 
-# Verifica se percorremos toda a SB
+#Verifica se percorremos toda a SB
 if j == B:
     print('S')
 else:
